1080160a1ee9bcd04ddca989999cdd41183fd679
Alistair Spragg <alistair.spragg@gmail.com>
2021-12-19 18:42:19 +0000

    Implement drag event

--- src/components/draggable.vue
+++ src/components/draggable.vue
@@ -1,34 +1,44 @@
 <script setup lang="ts">
-import { h, useSlots, ref, Ref, defineEmits } from 'vue'
+import { h, useSlots, ref, defineEmits } from 'vue'
+import { Ref } from 'vue' // Type declarations
+import DragEvent from '@/utils/VueDragEvent'
 
 const props = defineProps({
     element: {
         type: String,
         default: () => "div"
+    },
+
+    data:{
+        type: Object,
+        default: () => {}
     }
 })
 
-const isBeingDragged : Ref<Boolean> = ref(false);
+const isBeingDragged: Ref<Boolean> = ref(false);
 
 const slots = useSlots();
-const emit = defineEmits(["drag-start", "drag-end"])
+const emit = defineEmits(["drag-start", "drag-end", "drag-event"])
 
 const draggable = () => {
-    return h(props.element, {
+    let el = h(props.element, {
         draggable: true,
-        class: [isBeingDragged.value ? 'dragging' : ''],
-        ondragstart: () => {
-            isBeingDragged.value = true;
-            emit("drag-start")
-        },
-        ondragend: () => {
-            isBeingDragged.value = false;
-            emit("drag-end")
-        }
-    }, slots);
+        class: [isBeingDragged.value ? 'dragging' : '']
+    }, slots)
+    el.props!.ondragstart = () => {
+        isBeingDragged.value = true;
+        emit("drag-start", el)
+        emit("drag-event", new DragEvent("start", props.data, el))
+    };
+    el.props!.ondragend = () => {
+        isBeingDragged.value = false;
+        emit("drag-end", el)
+        emit("drag-event", new DragEvent("stop", props.data, el))
+    };
+    return el;
 }
 </script>
 
 <template>
-  <draggable/>
+    <draggable />
 </template>
