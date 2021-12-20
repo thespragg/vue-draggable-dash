f07c70de11d45160eec4678f670dc45f179ab466
Alistair Spragg <alistair.spragg@gmail.com>
2021-12-20 01:13:16 +0000

    Create demo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <dropable>
-    <draggable>
-      <p>Hello world.</p>
+  <dropable class="container">
+    <draggable prop="key" class="draggable" :data="{ key: '1' }">
+      <p>1.</p>
     </draggable>
-    <draggable>
-      <p>Hello world 2.</p>
+    <draggable prop="key" class="draggable" :data="{ key: '2' }">
+      <p>2.</p>
+    </draggable>
+  </dropable>
+  <dropable class="container">
+    <draggable prop="key" class="draggable" :data="{ key: '3' }">
+      <p>3.</p>
+    </draggable>
+    <draggable prop="key" class="draggable" :data="{ key: '4' }">
+      <p>4.</p>
     </draggable>
   </dropable>
 </template>
@@ -12,3 +20,28 @@
 <script setup lang="ts">
 import { Draggable, Dropable } from './components'
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  width: 100%;
+  background-color: #333;
+  padding: 10px;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+}
+
+.draggable {
+  background-color: white;
+  margin-top: 10px;
+  padding: 10px;
+}
+
+.dragging {
+  background-color: rgba(255, 255, 255, 0.5);
+}
+</style>
