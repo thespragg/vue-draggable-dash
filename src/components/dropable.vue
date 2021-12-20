1b74a5f11faa60cbed829ab0881ddcc41ac29200
Alistair Spragg <alistair.spragg@gmail.com>
2021-12-20 00:25:45 +0000

    Complete basic dropable

--- src/components/dropable.vue
+++ src/components/dropable.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { h, useSlots, ref } from 'vue'
-import DragEvent from '@/utils/VueDragEvent'
 
 const props = defineProps({
     element: {
@@ -9,44 +8,21 @@ const props = defineProps({
     }
 })
 
-const isDraggableComponent = (el: any) => {
-    try {
-        if (!el || !el.type || !el.type.emits || el.type.emits.length == 0) return false;
-        if (el.type.emits.includes("drag-event")) return true;
-        return false;
-    } catch {
-        return false;
-    }
-}
-
-const currentDragged = ref(null);
-const handleDrag = (ev: DragEvent) => {
-    if (ev.type == "start") {
-        currentDragged.value = ev.sender;
-    } else if (ev.type == "stop") {
-        currentDragged.value = null;
-    }
-    console.log(currentDragged.value)
-}
-
 const slots = useSlots();
 const dropcontainer = ref(null);
-
-const getChildren = () => {
-    if (!slots.default) return;
-    let els = slots.default();
-    for (let i = 0; i < els.length; i++) {
-        if (isDraggableComponent(els[i])) {
-            if (!els[i].props) els[i].props = {}
-            els[i].props!.onDragEvent = (ev: any) => handleDrag(ev)
-        }
-    }
-    return els;
-}
-
 const dropable = () => {
-    let children = getChildren();
-    let el = h(props.element, { ref: dropcontainer }, children)
+    let el = h(props.element, {
+        ref: dropcontainer,
+        ondrop: (ev : any) => {
+            ev.preventDefault();
+            let item = JSON.parse(ev.dataTransfer.getData("dragged"))
+            if(dropcontainer.value == null) return;
+            console.log(item.el)
+            let node = document.getElementById(item.el);
+            dropcontainer.value.appendChild(node)
+        },
+        ondragover: (e : any) => e.preventDefault()
+    }, slots)
     return el
 }
 </script>
